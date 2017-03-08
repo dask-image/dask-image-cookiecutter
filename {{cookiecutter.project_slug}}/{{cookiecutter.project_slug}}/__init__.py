4c7cfd30352e4b10e17891d418f9315220bdd499
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-03-08 17:46:44 -0500

    Fix quoting of author info around `__init__.py`.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = """{{ cookiecutter.full_name }}"""
-__email__ = '{{ cookiecutter.email }}'
+__email__ = "{{ cookiecutter.email }}"
 
 from ._version import get_versions
 __version__ = get_versions()['version']
